c42b715019adb947229a673540dc87784820505e
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-05 19:30:26 +0200

    add gallery library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,5 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
