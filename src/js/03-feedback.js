7f9cd80e01dfa5a9ec33196ccd9c18ce7f17fc0a
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-05 22:01:07 +0200

    feeeeedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -64,3 +64,40 @@ form.addEventListener('submit', (event) => {
 // }
 // );
 
+// 2.2
+// const STORAGE_KEY = 'feedback-form-state';
+// const form = document.querySelector('.feedback-form');
+// const allData = {};
+
+// form.addEventListener('submit', onFormSubmit);
+// form.addEventListener('input', throttle(onFormData, 500));
+
+// function onFormData(event) {
+//     allData[event.target.name] = event.target.value;   
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(allData));
+// }
+
+// function onFormSubmit(event) {
+//     event.preventDefault();    
+//     localStorage.removeItem(STORAGE_KEY);
+//     const allElements = event.currentTarget.elements;
+//     const formAll = {
+//         email: allElements.email.value,
+//         password: allElements.message.value,
+//     };
+//     console.log(formAll);
+//     event.currentTarget.reset();
+// };
+
+// function populateAllData() {
+//     const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//     const email = document.querySelector('.feedback-form input');
+//     const message = document.querySelector('.feedback-form textarea');
+//     if (data.email) {
+//       email.value = data.email;
+//     };
+//     if (data.message) {
+//       message.value = data.message;
+//     };
+// };
+// populateAllData();
